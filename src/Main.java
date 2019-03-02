fb4a84e91b61e86ff21256c8f794ba749f9c8e97
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2019-03-02 21:08:20 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import Tasks.Number178;
+import Tasks.Number554;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,Class<? extends Runnable>> numbersTasks=new HashMap<>();
+        numbersTasks.put("178", Number178.class);
+        numbersTasks.put("554",Number554.class);
+
+        System.out.println("Available tasks: ");
+        numbersTasks.keySet().forEach(System.out::println);
+
+        System.out.println("Number: ");
+        Scanner scanner=new Scanner(System.in);
+        String number=scanner.nextLine();
+
+        try {
+            numbersTasks.get(number).getConstructor().newInstance().run();
+        } catch (Exception ex) {}
+
+    }
+}
